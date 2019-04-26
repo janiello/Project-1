b69580ebebefc47b1ed1205c29f2153575f004fe
NChislom <11775893+nchislom@users.noreply.github.com>
2019-04-26 18:29:11 -0400

    Further build-out of functions. topArtists() and topTracks() now utilize jquery to pre-format content before passing to front-end

--- lastfm.js
+++ lastfm.js
@@ -1,4 +1,3 @@
-// API Key variable
 var api_key = "84a97eeb2d1c42b946ac60b243be2b7c";
 
 function getArtistInfo(artistName) {
@@ -21,17 +20,15 @@ function getArtistInfo(artistName) {
 }
 
 function getArtistPhoto(artistName) {
-    // Replace Spaces with URL friendly char encoding
+    // Replace spaces in names with "%20"
     artistName = artistName.replace(/ /g,"%20");
     
-    // Query Builder
     let queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=";
     queryURL += artistName;
     queryURL += "&api_key=";
     queryURL += api_key;
     queryURL += "&format=json";
     
-    // AJAX call
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -42,22 +39,17 @@ function getArtistPhoto(artistName) {
 }
 
 function getAlbumTracks(artistName, albumName) {
-    // Query Builder   
+    
     let queryURL = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=";
     queryURL += api_key;
     queryURL += "&artist=";
-    
-    // Replace Spaces with URL friendly char encoding
     artistName = artistName.replace(/ /g,"%20");
     queryURL += artistName;
     queryURL += "&album=";
-    
-    // Replace Spaces with URL friendly char encoding
     albumName = albumName.replace(/ /g,"%20");
     queryURL += albumName;
     queryURL += "&format=json";
 
-    // AJAX Call
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -72,6 +64,70 @@ function getAlbumTracks(artistName, albumName) {
     });
 }
 
-function getTrackInfo(artistName, trackName) {
+function getTopTracks() {
+    // Number of tracks to fetch
+    let limit = 20;
+
+    // Query Builder
+    let queryURL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks";
+    queryURL += "&limit=";
+    queryURL += limit;
+    queryURL += "&api_key="
+    queryURL += api_key;
+    queryURL += "&format=json";
+
+    // AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.tracks.track);
+        let newList = $("<ol>");
+        for(let i=0; i<response.tracks.track.length; i++) {
+            console.log(
+                i+1 + '. ' + response.tracks.track[i].artist.name + ' - ' + response.tracks.track[i].name   
+            );
+            let newItem = $("<li>").text(response.tracks.track[i]);
+            newList.append(newItem);
+        }
+        $("#top-tracks").append(newList);
+    });
+}
+
+function getTopArtists() {
+    // Number of artists to fetch
+    let limit = 10;
     
+    // Query Builder
+    let queryURL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists";
+    queryURL += "&limit=";
+    queryURL += limit;
+    queryURL += "&api_key="
+    queryURL += api_key;
+    queryURL += "&format=json";
+
+    // AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.artists.artist);
+        let newList = $("<ol>");
+        for(let i=0; i<response.artists.artist.length; i++) {
+            let newItem = $("<li>")
+            let newSpan = $("<span>")
+                .attr({
+                    "class": "artist",
+                    "data-artist": response.artists.artist[i].name
+                })
+                .text(response.artists.artist[i].name);
+            newItem.append(newSpan);
+            newList.append(newItem);
+        }
+        $("#top-artists").append(newList);
+
+        $(".artist").on("click", function(){
+            console.log($(this).attr("data-artist"));
+        });
+    });
 }
